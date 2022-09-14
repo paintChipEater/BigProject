c807b9a1e91b04527bd893deacf7187e05fc3de2
Saul Castillo <saulcastillo2006@gmail.com>
2022-09-13 23:16:32 -0700

    Backend runs now :) - Thinking about how to structure API

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const connectDB = require('./config/connectDB')
+const logger = require('./middleware/httpLogger')
+
+//Connect to local DB
+connectDB();
+
+//Some middleware
+app.use(logger);
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
 });
